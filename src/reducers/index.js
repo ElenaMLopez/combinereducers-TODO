5bfe2528643fde703b5d36b484584cceb3171a68
elenamlopez <elena.mateos1@gmail.com>
2020-04-05 13:45:52 +0200

    Cobine reducers  whith combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,17 @@ import { combineReducers } from 'redux';
 import ordenes from './ordenes';
 import usuarios from './usuarios'; 
 
-function reducer(state = {}, action) {
-  return {
-    ordenes: ordenes(state.ordenes, action),
-    usuarios: usuarios(state.usuarios, action)
-  };
-}
+// function reducer(state = {}, action) {
+//   return {
+//     ordenes: ordenes(state.ordenes, action),
+//     usuarios: usuarios(state.usuarios, action)
+//   };
+// }
 
+const reducer = combineReducers({
+  ordenes, 
+  usuarios
+});
  
 
 
